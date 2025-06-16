4e7fefce7852c4b2861926aafea6692ada604398
Dmitriy <Last-off@yandex.ru>
2025-06-16 23:48:09 +0300

    WiFi connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,55 @@
 #include <Arduino.h>
+#include <WiFi.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Подключаем наши учетные данные
+#include "wifi_data.h"
+
+void connectToWiFi() {
+  Serial.println("Подключение к Wi-Fi...");
+  
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  
+  Serial.print("SSID: ");
+  Serial.println(WIFI_SSID);
+  
+  int attempts = 0;
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    attempts++;
+    
+    if (attempts > 30) { // 15 секунд
+      Serial.println("\nОшибка подключения! Перезагрузка...");
+      delay(2000);
+      ESP.restart();
+    }
+  }
+  
+  Serial.println("\nУспешное подключение!");
+  Serial.print("IP адрес: ");
+  Serial.println(WiFi.localIP());
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  delay(2000); // Даем время для инициализации
+  
+  connectToWiFi();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // Проверка соединения каждые 10 секунд
+  static unsigned long lastCheck = 0;
+  
+  if (millis() - lastCheck > 10000) {
+    if (WiFi.status() == WL_CONNECTED) {
+      Serial.println("Wi-Fi подключение активно");
+    } else {
+      Serial.println("Потеряно соединение Wi-Fi! Переподключение...");
+      WiFi.reconnect();
+    }
+    lastCheck = millis();
+  }
+  
+  delay(1000);
 }
