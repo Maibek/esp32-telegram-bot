ebc540ded181a3e0dff9bfbb58a5060300f628cd
Dmitriy <Last-off@yandex.ru>
2025-06-17 23:12:16 +0300

    Adding logic to the telegram bot

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>
 
-// Подключаем наши учетные данные
+// Данные для подключения к Wi-Fi и токен бота
 #include "wifi_data.h"
+#include "token_bot.h"
+
+const unsigned long BOT_MTBS = 1000;
+const int LED_PIN = 2;
+
+WiFiClientSecure client;
+UniversalTelegramBot bot(TOKEN_BOT, client);
+unsigned long bot_lasttime = 0;
 
 void connectToWiFi() {
   Serial.println("Подключение к Wi-Fi...");
@@ -19,7 +29,7 @@ void connectToWiFi() {
     attempts++;
     
     if (attempts > 30) { // 15 секунд
-      Serial.println("\nОшибка подключения! Перезагрузка...");
+      Serial.println("\nПревышено ожидание. Перезапуск соединения...");
       delay(2000);
       ESP.restart();
     }
@@ -30,15 +40,43 @@ void connectToWiFi() {
   Serial.println(WiFi.localIP());
 }
 
+void handleNewMessages(int numNewMessages)
+{
+  for (int i = 0; i < numNewMessages; i++)
+  {
+    bot.sendMessage(bot.messages[i].chat_id, bot.messages[i].text, "New");
+
+    if (bot.messages[i].text == "/control") {
+      String keyboardJson = "[[\"/led_on\", \"/led_off\"]]";
+      bot.sendMessageWithReplyKeyboard(bot.messages[i].chat_id, "Выберите действие:", "", keyboardJson, true);
+}
+
+    if (bot.messages[i].text.equalsIgnoreCase("/led_on")) {
+      digitalWrite(LED_PIN, HIGH);
+      bot.sendMessage(bot.messages[i].chat_id, "Светодиод включен!");
+    }
+    else if (bot.messages[i].text.equalsIgnoreCase("/led_off")) {
+      digitalWrite(LED_PIN, LOW);
+      bot.sendMessage(bot.messages[i].chat_id, "Светодиод выключен!");
+    }
+  }
+}
+
 void setup() {
   Serial.begin(115200);
-  delay(2000); // Даем время для инициализации
-  
+  delay(2000); // Время для инициализации
+  client.setCACert(TELEGRAM_CERTIFICATE_ROOT);
+  pinMode(LED_PIN, OUTPUT);
+
   connectToWiFi();
+
+  digitalWrite(LED_PIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_PIN, LOW);
 }
 
 void loop() {
-  // Проверка соединения каждые 10 секунд
+  // Проверка соединения каждые 10 секунд. Необходимо в будущем поменять согласно будущему режиму сна и минимализации энергопотребления.
   static unsigned long lastCheck = 0;
   
   if (millis() - lastCheck > 10000) {
@@ -50,6 +88,18 @@ void loop() {
     }
     lastCheck = millis();
   }
-  
-  delay(1000);
+
+  if (millis() - bot_lasttime > BOT_MTBS)
+  {
+    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+
+    while (numNewMessages){
+      Serial.println("got response");
+      handleNewMessages(numNewMessages);
+      numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+    }
+
+    bot_lasttime = millis();
+    Serial.println("Free heap: " + String(ESP.getFreeHeap()));
+  }
 }
